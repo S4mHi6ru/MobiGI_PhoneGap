67b623f108e0ae2c628761ff5a488b7d397d55e7
S4mHi6ru <larec91@gmail.com>
2017-05-04 11:24:34 +0200

    Update tutorial app

--- App_Workshop/js/main.js
+++ App_Workshop/js/main.js
@@ -16,7 +16,15 @@ var app = {
     initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK')
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
 };
 
