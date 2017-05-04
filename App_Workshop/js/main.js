0e4468fa4572d2db0d6b7cb5806b33c16de82481
S4mHi6ru <larec91@gmail.com>
2017-05-04 11:46:38 +0200

    Update tutorial app       Part 4

--- App_Workshop/js/main.js
+++ App_Workshop/js/main.js
@@ -21,12 +21,22 @@ var app = {
         });
     },
 
+    renderHomeView: function () {
+        var html=
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function () {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
