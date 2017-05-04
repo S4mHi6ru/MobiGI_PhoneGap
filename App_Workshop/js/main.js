06c763408a6bf011fc530ecc0f0ddd91111648c3
S4mHi6ru <larec91@gmail.com>
2017-05-04 11:42:07 +0200

    Update tutorial app Part 3

--- App_Workshop/js/main.js
+++ App_Workshop/js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK')
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,17 +22,12 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function () {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    showAlert: function (message, title) {
-        if (navigator.notification){
-            navigator.notification.alert(message, null, title, 'OK')
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
+    }
 
 };
 
